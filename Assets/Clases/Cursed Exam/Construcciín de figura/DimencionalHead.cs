6f840e2fb3637092b74f797540fcaaa4f89b259d
Angel Legorreta <legosurio@hotmail.com>
2021-05-05 01:03:07 -0500

    Cabeza completa

--- Assets/Clases/Cursed Exam/Construcciín de figura/DimencionalHead.cs
+++ Assets/Clases/Cursed Exam/Construcciín de figura/DimencionalHead.cs
@@ -30,18 +30,18 @@ public class DimencionalHead : MonoBehaviour
         {
             // Cube 1A0
             new Vector3(0,0.5f,0), //0
-            new Vector3(-1.45f,4,1.45f), //1
-            new Vector3(1.45f,4,1.45f), //2
-            new Vector3(0,11.95f,0), //3
-            new Vector3 (1.45f,4,-1.45f), //4
-            new Vector3 (-1.45f,4,-1.45f), //5
+            new Vector3(-1.30f,4,1.3f), //1
+            new Vector3(1.3f,4,1.3f), //2
+            new Vector3(0,11.8f,0), //3
+            new Vector3 (1.3f,4,-1.3f), //4
+            new Vector3 (-1.3f,4,-1.3f), //5
         };
 
         meshFilter.mesh.vertices = vertex;
 
         triangles = new[]
         {
-            //Cabeza
+            //CabezaD
             0,1,2,
             2,1,3,
             0,2,4,
---
+++ Assets/Clases/Cursed Exam/Construcciín de figura/Ojos.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ojos : MonoBehaviour
+{
+    Mesh mesh;
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
+    public Material material;
+
+    Vector3[] vertex;
+    int[] triangles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triangles = new int[4];
+
+
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+        meshRenderer.material = material;
+
+        meshFilter.mesh = mesh;
+
+        vertex = new[]
+        {
+            new Vector3(0,-1,0), //0
+            new Vector3(-0.2f,0,0.2f), //1
+            new Vector3(0.2f,0,0.2f), //2
+            new Vector3(0,1,0), //3
+            new Vector3 (0.2f,0,-0.2f), //4
+            new Vector3 (-0.2f,0,-0.2f), //5
+        };
+
+        meshFilter.mesh.vertices = vertex;
+
+        triangles = new[]
+        {
+            //Ojos
+            2,1,0,
+            2,3,1,
+            4,2,0,
+            3,2,4,
+            5,0,1,
+            1,3,5,
+            4,0,5,
+            4,5,3
+        };
+
+        meshFilter.mesh.triangles = triangles;
+    }
+
+
+
+}
